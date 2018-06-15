33269206abab5a1ba1c14678103f640bfeccd36b
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-15 01:19:41 +0000

    automatically backed up by learn

---
+++ jamz.rb
@@ -0,0 +1,25 @@
+class Album
+  
+  @@album_counter= 0 
+  
+  
+  @@album_counter += 1 
+  
+  def release_date=(date) #setters
+    @release_date = date 
+  end
+  
+  def release_date 
+    @release_date
+  end 
+  
+end
+
+kod = Album.new  #getters
+kod.release_date = "April 2018"
+
+ye = Album.new 
+ye.release_date = "June 2018"
+
+shawn_mendes = Album.new 
+shawn_mendes.release_date = "May 2018"
